6bded704b8861625a3687e22dbd7eb8cba31a57d
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-21 15:51:03 +0530

    add insert and find method with iterative

--- bst.js
+++ bst.js
@@ -58,25 +58,7 @@ class BinarySearchTree{
     }
     
 
-    find(value){
-        if(this.root === null) return false;
-
-        let current = this.root;
-        let found= false;
-
-        while(current && !found){
-            if(value < current.value){
-                current=current.left;
-            }else if(value > current.value){
-                current=current.right;
-            }else{
-                return true;
-            }
-        }
-
-        return false;
-
-    }
+   
 }
         
 
